90608531b7f734de26c7aa5ac36bb7227f7c279f
Antonio Marin Jimenez <antonio.marin.jimenez@gmail.com>
2020-07-14 16:59:20 +0200

    Formatting code

--- app/index.js
+++ app/index.js
@@ -7,7 +7,7 @@ var yosay = require('yosay');
 var jsesc = require('jsesc');
 
 function jsonEscape(str) {
-  return jsesc(str, {quotes: 'double'});
+  return jsesc(str, { quotes: 'double' });
 }
 
 module.exports = generators.Base.extend({
@@ -24,9 +24,7 @@ module.exports = generators.Base.extend({
 
   prompting: function() {
     // Have Yeoman greet the user.
-    this.log(yosay(
-      'Welcome to the ' + chalk.red('Cartridge Lib Module') + ' generator!'
-    ));
+    this.log(yosay('Welcome to the ' + chalk.red('Cartridge Lib Module') + ' generator!'));
 
     var gitName = this.user.git.name();
     var gitEmail = this.user.git.email();
@@ -35,58 +33,65 @@ module.exports = generators.Base.extend({
       defaultAuthor += ` <${gitEmail}>`;
     }
 
-    var prompts = [{
-      type: 'input',
-      name: 'module_name',
-      message: 'What is the name of this library module?',
-      default: this.defaultModuleName
-    }, {
-      type: 'input',
-      name: 'module_version',
-      message: 'What is the version of the node module dependency?',
-      default: this.defaultModuleVersion
-    },{
-      type: 'input',
-      name: 'module_type',
-      message: 'What is the main folder of the node module dependency on package.json (lib, dist, ...)?',
-      default: this.defaultModuleType
-    }, {
-      type: 'input',
-      name: 'user',
-      message: 'What is the Github username/organization for this project?',
-      default: this.username,
-      store: true
-    }, {
-      type: 'input',
-      name: 'repo',
-      message: 'What is the repository/project name?',
-      // This default works when the directory was generated from a cloned
-      // repository, or when the user intends to make a repo with the same
-      // name as the directory. We assume this is the most common situation.
-      default: this.dirname
-    }, {
-      type: 'input',
-      name: 'description',
-      message: 'What is a short description for this project?'
-    }, {
-      type: 'input',
-      name: 'author',
-      message: 'Who is the author of this project?',
-      default: defaultAuthor,
-      store: true
-    }];
+    var prompts = [
+      {
+        type: 'input',
+        name: 'module_name',
+        message: 'What is the name of this library module?',
+        default: this.defaultModuleName
+      },
+      {
+        type: 'input',
+        name: 'module_version',
+        message: 'What is the version of the node module dependency?',
+        default: this.defaultModuleVersion
+      },
+      {
+        type: 'input',
+        name: 'module_type',
+        message: 'What is the main folder of the node module dependency on package.json (lib, dist, ...)?',
+        default: this.defaultModuleType
+      },
+      {
+        type: 'input',
+        name: 'user',
+        message: 'What is the Github username/organization for this project?',
+        default: this.username,
+        store: true
+      },
+      {
+        type: 'input',
+        name: 'repo',
+        message: 'What is the repository/project name?',
+        // This default works when the directory was generated from a cloned
+        // repository, or when the user intends to make a repo with the same
+        // name as the directory. We assume this is the most common situation.
+        default: this.dirname
+      },
+      {
+        type: 'input',
+        name: 'description',
+        message: 'What is a short description for this project?'
+      },
+      {
+        type: 'input',
+        name: 'author',
+        message: 'Who is the author of this project?',
+        default: defaultAuthor,
+        store: true
+      }
+    ];
 
     var self = this;
-    return self.prompt(prompts)
-      .then(function(props) {
-        self.user = jsonEscape(props.user);
-        self.repo = jsonEscape(props.repo);
-        self.description = jsonEscape(props.description);
-        self.author = jsonEscape(props.author);
-        self.module_name = props.module_name;
-        self.module_version = props.module_version;
-        self.module_type = props.module_type;
-      });
+    return self.prompt(prompts).then(function(props) {
+      self.user = jsonEscape(props.user);
+      self.repo = jsonEscape(props.repo);
+      self.description = jsonEscape(props.description);
+      self.author = jsonEscape(props.author);
+      self.module_name = props.module_name;
+      self.module_version = props.module_version;
+      self.module_type = props.module_type;
+    });
   },
 
   writing: {
@@ -111,8 +116,14 @@ module.exports = generators.Base.extend({
       this.template('gitignore', '.gitignore');
       this.template('cartridges/lib_module', 'cartridges/lib_' + self.module_name);
       this.template('cartridges/lib_module/.project', 'cartridges/lib_' + self.module_name + '/.project');
-      this.template('cartridges/module', 'cartridges/lib_' + self.module_name + '/cartridge/scripts/lib/' + self.module_name);
-      this.template('cartridges/lib_module.properties', 'cartridges/lib_' + self.module_name + '/cartridge/lib_' + self.module_name + '.properties');
+      this.template(
+        'cartridges/module',
+        'cartridges/lib_' + self.module_name + '/cartridge/scripts/lib/' + self.module_name
+      );
+      this.template(
+        'cartridges/lib_module.properties',
+        'cartridges/lib_' + self.module_name + '/cartridge/lib_' + self.module_name + '.properties'
+      );
     }
   },
 
@@ -121,11 +132,11 @@ module.exports = generators.Base.extend({
       bower: false,
       npm: true,
       skipInstall: this.options['skip-install']
-    }
+    };
     if (!installConfig.skipInstall) {
-      installConfig.callback = function () {
+      installConfig.callback = function() {
         this.spawnCommand('npm', ['run', 'build']);
-      }.bind(this) // bind the callback to the parent scope
+      }.bind(this); // bind the callback to the parent scope
     }
     this.installDependencies(installConfig);
   }
--- test/test.js
+++ test/test.js
@@ -5,19 +5,17 @@ var assert = require('yeoman-assert');
 var helpers = require('yeoman-test');
 var os = require('os');
 
-describe('cartridge-lib-module:app', function () {
-  before(function (done) {
+describe('cartridge-lib-module:app', function() {
+  before(function(done) {
     console.log('Generating test template on ' + os.tmpdir() + '/lib-temp-test');
-    helpers.run(path.join(__dirname, '../app'))
+    helpers
+      .run(path.join(__dirname, '../app'))
       .inDir(path.join(os.tmpdir(), './lib-temp-test'))
       .withOptions({ 'skip-install': true })
       .on('end', done);
   });
 
-  it('creates files', function () {
-    assert.file([
-      'package.json',
-      '.eslintrc.json'
-    ]);
+  it('creates files', function() {
+    assert.file(['package.json', '.eslintrc.json']);
   });
 });
